1733b41e407148dccdb7b199085ae5ccf7dceb0e
arabelhousseyn <hocine.arab1@hotmail.com>
2022-05-28 19:56:04 +0100

    make some changes

--- app/Events/OrderConfirmedForSeller.php
+++ app/Events/OrderConfirmedForSeller.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class OrderConfirmedForSeller
+class OrderConfirmedForSeller implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     /**
