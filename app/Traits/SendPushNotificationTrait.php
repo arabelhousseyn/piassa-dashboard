5148037afca81254e6a1e3c79c14745c3216cb05
arabelhousseyn <hocine.arab1@hotmail.com>
2022-06-11 08:57:46 +0100

    add try and catch into the function push

--- app/Traits/SendPushNotificationTrait.php
+++ app/Traits/SendPushNotificationTrait.php
@@ -9,31 +9,36 @@ trait SendPushNotificationTrait
 {
     public function push($title,$message,$user_id)
     {
-        $tokens = array();
-        $user = User::with('profile')->find($user_id);
-        $tokens[] = $user->profile->device_token;
-        $SERVER_API_KEY = env('SERVER_KEY_TARGET_USERS');
-        $data = [
-            "registration_ids" => $tokens,
-            "notification" => [
-                "title" => $title,
-                "body" => $message,
-                'image' => '',
-                'sound' => true,
-            ]
-        ];
-        $dataString = json_encode($data);
-        $headers = [
-            'Authorization: key=' . $SERVER_API_KEY,
-            'Content-Type: application/json',
-        ];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-        return curl_exec($ch);
+        try {
+            $tokens = array();
+            $user = User::with('profile')->find($user_id);
+            $tokens[] = $user->profile->device_token;
+            $SERVER_API_KEY = env('SERVER_KEY_TARGET_USERS');
+            $data = [
+                "registration_ids" => $tokens,
+                "notification" => [
+                    "title" => $title,
+                    "body" => $message,
+                    'image' => '',
+                    'sound' => true,
+                ]
+            ];
+            $dataString = json_encode($data);
+            $headers = [
+                'Authorization: key=' . $SERVER_API_KEY,
+                'Content-Type: application/json',
+            ];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+            return curl_exec($ch);
+        }catch (\Exception $exception)
+        {
+
+        }
     }
 }
